e585b2d3a858af73c1da31c973822768d8a71b9f
lexvieira <lexvieira.info@gmail.com>
2020-06-10 02:23:08 +0700

    Initial Asp.Net Core Angular

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using C_ProgrammingGuide.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_ProgrammingGuide.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace C_ProgrammingGuide.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
